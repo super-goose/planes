63b9fd58bd9796d8bb62816390e975f6159e55a6
Jay <theworldneedssupergoose@gmail.com>
2014-07-23 23:54:42 -0500

    update after normalization

--- glwidget.h
+++ glwidget.h
@@ -3,9 +3,17 @@
 
 #include <QGLWidget>
 #include <QTimer>
+#include "objloader.h"
 
 class GLWidget : public QGLWidget
 {
+public:
+    struct vector{
+        float x;
+        float y;
+        float z;
+    };
+
     Q_OBJECT
 public:
     explicit GLWidget(QWidget *parent = 0);
@@ -17,12 +25,15 @@ public:
 private:
     QTimer timer;
 
-private:
-    float s;  // scale for object   (1/s is size)
+    float s; // scale for object (1/s is size)
+    vector set_vector(objloader::point main, objloader::point pt1, objloader::point pt2);
+    vector cross(vector v1, vector v2);
+
 public:
     float zo; // offset for z coord (- is away from user)
     float yo;
     float xo;
+
 
 };
 
